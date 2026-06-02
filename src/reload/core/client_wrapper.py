ffb4b662dd96f27bff0795e2bceb9c17775214e0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-02 08:43:39 +0000

    SDK regeneration

--- src/reload/core/client_wrapper.py
+++ src/reload/core/client_wrapper.py
@@ -22,9 +22,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
+            "User-Agent": "reload-sdk/1.0.0",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "reload-sdk",
-            "X-Fern-SDK-Version": "0.0.9",
+            "X-Fern-SDK-Version": "1.0.0",
             **(self.get_custom_headers() or {}),
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
